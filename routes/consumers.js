b901c873b28e1a63808b99da908649d2296bddc3
ivanmakarov.7ch <ivanmak.7ch@gmail.com>
2025-09-19 22:19:07 +0300

    Fixed Routes

--- routes/consumers.js
+++ routes/consumers.js
@@ -1,29 +1,31 @@
-const express = require('express');
-const {
-  getConsumers,
-  getConsumerById,
-  createConsumer,
-  updateConsumer,
-  deleteConsumer
-} = require('../controllers/consumerController');
+const { body, validationResult } = require('express-validator');
 
-const { consumerValidationRules, validate } = require('../validators/consumerValidator');
+// Rules for creating/updating consumers
+const consumerValidationRules = () => {
+  return [
+    body('firstName').notEmpty().withMessage('First name is required'),
+    body('lastName').notEmpty().withMessage('Last name is required'),
+    body('email').isEmail().withMessage('Valid email is required'),
+    body('phone').optional().isMobilePhone().withMessage('Phone must be valid'),
+    body('address').optional().isString().withMessage('Address must be a string'),
+    body('preferredFlavor').optional().isString().withMessage('Preferred flavor must be a string'),
+    body('allergies').optional().isString().withMessage('Allergies must be a string'),
+    body('orderHistory').optional().isArray().withMessage('Order history must be an array')
+  ];
+};
 
-const router = express.Router();
+// Middleware to check validation results
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) return next();
 
-// GET all consumers
-router.get('/', getConsumers);
+  const extractedErrors = [];
+  errors.array().map(err => extractedErrors.push({ [err.param]: err.msg }));
 
-// GET consumer by ID
-router.get('/:id', getConsumerById);
+  return res.status(422).json({ errors: extractedErrors });
+};
 
-// POST new consumer
-router.post('/', consumerValidationRules(), validate, createConsumer);
-
-// PUT update consumer
-router.put('/:id', consumerValidationRules(), validate, updateConsumer);
-
-// DELETE consumer
-router.delete('/:id', deleteConsumer);
-
-module.exports = router;
+module.exports = {
+  consumerValidationRules,
+  validate
+};
